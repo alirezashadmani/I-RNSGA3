a2cffd962b3b520d165249e3356eaa0131406509
alirezashadmani <alirezashademani33@gmail.com>
2023-01-24 13:46:57 +0100

    seq2seq model preparation

--- wave_pred.py
+++ wave_pred.py
@@ -1,13 +1,10 @@
+######################### IMPORT LIBRARIES #########################
+'''
+Implementation of Seq2Seq Model for Wave Power Prediction
+'''
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sn
 import plotly.express as px
 import plotly.graph_objects as go
-import windrose
-import tensorflow as tf
-from sklearn.ensemble import RandomForestClassifier
-import pymoo, deap, pyomo
-
-import io, pathlib
-from scipy.io import loadmat
