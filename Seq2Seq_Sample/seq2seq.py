fc45c6b40626f893655e2889ff1c4a7e1c55372b
alirezashadmani <alirezashademani33@gmail.com>
2023-01-24 14:57:36 +0100

    seq2seq.py finalized

--- Seq2Seq_Sample/seq2seq.py
+++ Seq2Seq_Sample/seq2seq.py
@@ -51,7 +51,71 @@ def tokenize(lang):
     return tensor, lang_tokenizer
 
 def load_dataset(path, num_examples = None):
+
     targ_lang, inp_lang = create_dataset(path, num_examples)
     input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
     target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
     return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
+
+
+class Encoder(tf.keras.Model):
+
+    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+
+        super(Encoder, self).__init__()
+        self.batch_sz = batch_sz
+        self.enc_units = enc_units
+        self.embedding = tf.keras_layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.enc_units,
+                                       return_sequence = True,  # Whether to return the last output in the output sequence, or the full sequence.
+                                       return_state = True, # Whether to return the last state in addition to the output.
+                                       recurrent_initializer = 'glorot_uniform')
+        
+    def call(self, x, hidden):
+
+        x = self.embedding(x)
+        output, state = self.gru(x, initial_state = hidden)
+        return output, state
+    
+    def initialize_hidden_state(self):
+
+        return tf.zeros((self.batch_sz, self.enc_units))
+        
+
+
+class Decoder(tf.keras.Model):
+
+    def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
+
+        super(Decoder, self).__init__()
+        self.batch_sz = batch_sz
+        self.dec_units = dec_units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.dec_units,
+                                       return_sequence = True,
+                                       return_state = True,
+                                       recurrent_initializer = 'glorot_uniform')
+        self.fc = tf.keras.layers.Dense(vocab_size)
+
+    def call(self, x, hidden):
+
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+        x = self.embedding(x)
+
+        # passing the concatenated vector to the GRU
+        output, state = self.gru(x, initial_state = hidden)
+
+        # output shape == (batch_size * 1, hidden_size)
+        output = tf.reshape(output, (-1, output.shape[2]))
+
+        # output shape == (batch_size, vocab)
+        x = self.fc(output)
+
+        return x, state
+    
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True, reduction = 'none')
+
+def loss_function(real, pred):
+    
+    loss_ = loss_object(real, pred)
+    return tf.reduce_mean(loss_)
